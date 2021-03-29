a463e5178809abf5e54510f8d7e236a561945fe0
over <cjnavia@hotmail.com>
2021-03-29 20:51:05 +0200

    Creado Observer con expresiones regulares para añadir videos youtube y vimeo

--- resources/views/livewire/Instructor/courses-lesson.blade.php
+++ resources/views/livewire/Instructor/courses-lesson.blade.php
@@ -16,7 +16,7 @@
 
                 <div class="flex item-center mt-4">
                     <label class="w-32">Plataforma:</label>
-                    <select wire:model="lesson.platform_id">
+                    <select wire:model="lesson.platform_id" class="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                         @foreach ($platforms as $platform)
                             <option value="{{ $platform->id }}">{{ $platform->name }}</option>
                         @endforeach
@@ -32,6 +32,12 @@
                 @error('lesson.url')
                     <span class="text-red-500 text-xs">{{ $message }}</span>
                 @enderror
+
+                <div class="mt-4 flex justify-end">
+                    <button class="btn btn-danger text-xs" wire:click="cancel">Cancelar</button>
+                    <button class="btn btn-primary ml-2 text-xs" wire:click="update">Actualizar</button>
+                </div>
+
             @else
                 <header>
                     <h1><i class="far fa-play-circle text-blue-500 mr-1"></i> Lección: {{ $item->name }}</h1>
@@ -43,11 +49,63 @@
 
                     <div class="mt-2">
                         <button class="btn btn-primary text-sm" wire:click="edit({{ $item }})">Editar</button>
-                        <button class="btn btn-danger text-sm">Eliminar</button>
+                        <button class="btn btn-danger text-sm" wire:click="destroy({{ $item }})">Eliminar</button>
                     </div>
                 </div>
                 @endif
            </div>
        </article>
    @endforeach
+
+
+   <div class="mt-4" x-data="{open: false}">
+    <a x-show="!open" x-on:click="open = true" class="flex item-center cursor-pointer">
+        <i class="far fa-plus-square text-2xl text-red-500 mr-2"></i>
+        Agregar nueva Lección
+    </a>
+    <article class="card" x-show="open">
+        <div class="card-body">
+            <h1 class="text-xl font-bold mb-4">Agregar nueva Lección</h1>
+            <div>
+                <div>
+                    <div class="flex items-center">
+                        <label class="w-32">Nombre:</label>
+                        <input wire:model="name" type="text" class="mt-1 focus:ring-gray-400 focus:border-gray-400 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                </div>
+                @error('name')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
+
+                <div class="flex item-center mt-4">
+                    <label class="w-32">Plataforma:</label>
+                    <select wire:model="platform_id" class="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        @foreach ($platforms as $platform)
+                            <option value="{{ $platform->id }}">{{ $platform->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                @error('platform_id')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
+
+                <div>
+                    <div class="flex items-center mt-4">
+                        <label class="w-32">URL:</label>
+                        <input wire:model="url" type="text" class="mt-1 focus:ring-gray-400 focus:border-gray-400 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                </div>
+                @error('url')
+                    <span class="text-red-500 text-xs">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-2">
+                <button class="btn btn-danger" x-on:click="open = false">Cancelar</button>
+                <button class="btn btn-primary ml-2" wire:click="store">Agregar</button>
+            </div>
+
+        </div>
+    </article>
+</div>
 </div>
